2a22fca0023bc6af1900d6baa54433f6ccbebdf2
didemkkaslan <didem@spiky.ai>
2022-11-06 20:18:59 +0300

    Added chakra ui and installed fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+import "@fontsource/m-plus-rounded-1c";
+import "@fontsource/open-sans";
+
+import "../styles/globals.css";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "M PLUS Rounded 1c",
+    body: "Open Sans",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
