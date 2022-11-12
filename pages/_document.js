75492bf97c6671b4872f13377ed5ae0a2c53a5d0
didemkkaslan <didem@spiky.ai>
2022-11-12 18:34:11 +0300

    add buymeacoffee widget

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,21 @@ import { theme } from "@chakra-ui/react";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="codewithdidem"
+          data-description="Support me on Buy me a coffee!"
+          data-message="Thanks for this awesome course. Keep creating!"
+          data-color="#BD5FFF"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
